9e062fb906907f3ea38c7f4d242cb713b75a0d89
Abdurrahman Stjärnborg <apo.b@hotmail.se>
2024-09-25 22:16:47 +0200

    Get Token

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,19 +3,59 @@ const BASE_URL = 'http://localhost:3000';
 
 test.describe('Test suite', () => {
 
-  test('Test case LogIn', async ({ request }) => {
-    const getPostsResponse = await request.get('http://localhost:3000/');
-    expect(getPostsResponse.ok()).toBeTruthy();
-    expect(getPostsResponse.status()).toBe(200);
+  let tokenValue;
+  test.beforeAll('Test case LogInGetToken', async ({ request }) => {
+    const respToken = await request.post("http://localhost:3000/api/login", {
+      data: {
+        username: "tester01",
+        password: "GteteqbQQgSr88SwNExUQv2ydb7xuf8c"
+      }
+    })
+
+    tokenValue = (await respToken.json()).token;
+
   });
 
-  /*
-  test('test case 2 ', async ({ page }) => {
-    await page.goto('http://localhost:3000');
+  test('Test case 01 - Get all rooms', async ({ request }) => {
+    const respRooms = await request.get("http://localhost:3000/api/rooms", {
+      headers: {
+        "X-user-auth": JSON.stringify({
+          username: "tester01",
+          token: tokenValue
+        })
+      },
+    });
+
+    console.log(await respRooms.json())
+    expect(await respRooms.ok())
+
+  })
+
+
+
+
+});
+
+
+
+
+/*
+  test('Test case LogIn', async ({ request }) => {
+    const getPostsResponse = await request.get('http://localhost:3000/');
     await page.locator('input[type="text"]').fill('tester01');
     await page.locator('input[type="password"]').fill('GteteqbQQgSr88SwNExUQv2ydb7xuf8c');
-    await page.getByRole('button', { name: 'Login' }).click();
-
+    expect(getPostsResponse.ok()).toBeTruthy();
+    expect(getPostsResponse.status()).toBe(200);
   });*/
-});
+
+/*
+POST http://localhost:3000/api/login
+http://localhost:3000/login
+test('test case 2 ', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await page.locator('input[type="text"]').fill('tester01');
+  await page.locator('input[type="password"]').fill('GteteqbQQgSr88SwNExUQv2ydb7xuf8c');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+});*/
 
